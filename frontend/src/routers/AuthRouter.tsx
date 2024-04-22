0780bfd4aaae992ca568403f7228d0f45b38534a
유병욱 <tls19190@naver.com>
2024-04-22 23:42:26 +0900

    ⚙️ config : manifest 추가 탭 확인 후  페이지 라우팅

--- frontend/src/routers/AuthRouter.tsx
+++ frontend/src/routers/AuthRouter.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MyCard from "../pages/MyCard";
+import MyAlbum from "../pages/MyAlbum";
 
 const AuthRouter = () => {
   return (
     <Routes>
+      <Route path="/tercero" element={<MyAlbum />} />
+      <Route path="/newTab" element={<MyAlbum />} />
       <Route path="*" element={<MyCard />} />
     </Routes>
   );
