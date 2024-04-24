66d86e075afe50c260bf7458630846156651145d
김민주 <ducco705@snu.ac.kr>
2024-04-24 15:40:13 +0900

    #S10P31S208-149 🎨 style: 일부 아이콘 등록

--- frontend/src/pages/mobile/AppCard.tsx
+++ frontend/src/pages/mobile/AppCard.tsx
@@ -1,11 +1,30 @@
-import Text from '@/components/shared/Text'
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import Spacing from '@shared/Spacing'
+
+import { CameraAdd48Regular, Edit48Regular } from '@fluentui/react-icons'
+import { css } from '@emotion/react'
 
 const AppCard = () => {
   return (
-    <div>
+    <>
       <Text>모바일 내 명함</Text>
-    </div>
+      <Flex justify="space-around" css={iconsStyles}>
+        <Flex direction="column" align="center">
+          <CameraAdd48Regular />
+          <Text typography="t7">직접 촬영</Text>
+        </Flex>
+        <Flex direction="column" align="center">
+          <Edit48Regular />
+          <Text typography="t7">직접 입력</Text>
+        </Flex>
+      </Flex>
+    </>
   )
 }
+
+const iconsStyles = css`
+  margin: 0 auto;
+`
 
 export default AppCard
