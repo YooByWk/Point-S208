0b5057a3dffa5c75940194143e606a50fa4b51a6
unknown <tls19190@naver.com>
2024-04-24 14:11:30 +0900

    #S10P31S208-15 � style : 테마별 생상표 생성

--- frontend/src/components/Context.tsx
+++ frontend/src/components/Context.tsx
@@ -2,6 +2,10 @@ import { TeamsUserCredential } from "@microsoft/teamsfx";
 import { createContext } from "react";
 import { Theme } from "@fluentui/react-components";
 
+
+
+
+
 export const TeamsFxContext = createContext<{
   theme?: Theme;
   themeString: string;
--- frontend/src/pages/web/WebAlbum.tsx
+++ frontend/src/pages/web/WebAlbum.tsx
@@ -1,9 +1,10 @@
-
+import LargeButton from "@/components/shared/LargeButton"
 
 const WebAlbum = () => {
   return (
     <div>
       웹 명함 지갑
+      <LargeButton text="명함 추가" onClick={() => {console.log('혹시설마진짜?')}} bgColor="ssafyLightBlue" />
     </div>
   )
 }
