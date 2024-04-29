0c5601e77904117c6f9ae86ffe5e472bf30c7f43
강대수 <sore503@naver.com>
2024-04-29 14:33:57 +0900

    Update backend-cud/src/main/java/com/ssafy/businesscard/myalbum/controller/PrivateAlbumFilterController.java

--- backend-cud/src/main/java/com/ssafy/businesscard/myalbum/controller/PrivateAlbumFilterController.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/myalbum/controller/PrivateAlbumFilterController.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/my-album")
 public class PrivateAlbumFilterController {
-    private final
+
 }
