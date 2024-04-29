de9883a99a797acae0e4d8ae6c9ad55602433af5
유병욱 <tls19190@naver.com>
2024-04-29 23:02:31 +0900

    #S10P31S208-198 🐛 fix: 라우트 적용.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,6 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 const container = document.getElementById('root')
 const root = createRoot(container!)
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
--- frontend/src/pages/MyTeam.tsx
+++ frontend/src/pages/MyTeam.tsx
@@ -2,13 +2,24 @@
 import useWindowSize from "@/hooks/useWindowSize"
 import WebTeam from "./web/WebTeam"
 import AppTeam from "./mobile/AppTeam"
+import { Outlet, Routes } from "react-router-dom"
 
+/**
+ * @deprecated 
+ * 이 컴포넌트는 사용되지 않습니다.
+ * 삭제 예정입니다.
+ * @obsolete
+ * 이 컴포넌트는 사용되지 않습니다. 필요한 경우 AppTeam 또는 WebTeam을 사용하세요.
+ */ 
 const MyTeam = () => {
   const width = useWindowSize()
   
   return (
     <div >
       {width >= 768 ? <WebTeam /> : <AppTeam />}
+        {/* <Outlet/> */}
+      {/* <Routes> */}
+      {/* </Routes> */}
     </div>
   )
 }
