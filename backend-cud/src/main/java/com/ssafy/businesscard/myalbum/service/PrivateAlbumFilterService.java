3fc8b5b64db9d50f03d8a5fd9ef324f19585ded8
강대수 <sore503@naver.com>
2024-04-29 22:52:46 +0900

    #S10P31S208-211 ✨feat: 필터 이름 편집

--- backend-cud/src/main/java/com/ssafy/businesscard/myalbum/service/PrivateAlbumFilterService.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/myalbum/service/PrivateAlbumFilterService.java
@@ -4,4 +4,6 @@ import com.ssafy.businesscard.card.dto.request.FilterRequest;
 
 public interface PrivateAlbumFilterService {
     void create(Long userId, FilterRequest request);
+
+    void update(Long userId, Long filterId, FilterRequest request);
 }
