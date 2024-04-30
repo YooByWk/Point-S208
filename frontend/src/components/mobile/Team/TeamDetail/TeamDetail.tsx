759aa3b437baa37dd894ac2ca4c82188be764149
유병욱 <tls19190@naver.com>
2024-04-30 10:56:03 +0900

    #S10P31S208-209 🎨 style: 모바일 스크롤 제거

--- frontend/src/components/mobile/Team/TeamDetail/TeamDetail.tsx
+++ frontend/src/components/mobile/Team/TeamDetail/TeamDetail.tsx
@@ -1,9 +1,12 @@
+import BackArrow from '@/components/shared/BackArrow';
+import CardList from '@/components/shared/CardList';
 import React from 'react';
 
 const TeamDetail = () => {
   return (
     <div>
-      
+      <BackArrow/>
+      <CardList/>
     </div>
   );
 };
