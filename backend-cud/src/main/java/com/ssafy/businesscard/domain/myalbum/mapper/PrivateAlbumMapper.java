0ec81fb8690413a940fb4af0edaa362049361141
강대수 <sore503@naver.com>
2024-05-02 15:23:50 +0900

    #S10P31S208-216 ✨feat : 명함지갑 명함 삭제

--- backend-cud/src/main/java/com/ssafy/businesscard/domain/myalbum/mapper/PrivateAlbumMapper.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/domain/myalbum/mapper/PrivateAlbumMapper.java
@@ -3,10 +3,8 @@ package com.ssafy.businesscard.domain.myalbum.mapper;
 import com.ssafy.businesscard.domain.card.entity.Businesscard;
 import com.ssafy.businesscard.domain.myalbum.dto.request.UpdateCardRequest;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PrivateAlbumMapper {
-
     Businesscard toEntity(UpdateCardRequest updateCardRequest);
 }
