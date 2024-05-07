737fb7e0441329a0118940057e01250879546bbb
강대수 <sore503@naver.com>
2024-05-07 13:45:57 +0900

    #S10P31S208-311 ✨feat : 팀 명함지갑 명함에 메모 등록 및 수정

--- backend-cud/src/main/java/com/ssafy/businesscard/domain/team/service/TeamAlbumService.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/domain/team/service/TeamAlbumService.java
@@ -2,6 +2,7 @@ package com.ssafy.businesscard.domain.team.service;
 
 import com.ssafy.businesscard.domain.card.dto.request.CardAddFilterRequest;
 import com.ssafy.businesscard.domain.card.dto.request.CardRequest;
+import com.ssafy.businesscard.domain.card.dto.request.MemoRequest;
 import com.ssafy.businesscard.domain.team.dto.request.TeamAlbumRegistRequest;
 
 import java.util.List;
@@ -21,4 +22,6 @@ public interface TeamAlbumService {
     void deleteCard(Long teamAlbumId, Long cardId);
 
     void addFilter(Long teamAlbumId, Long cardId, List<CardAddFilterRequest> requestList);
+
+    String cardMemo(Long teamAlbumId, Long cardId, MemoRequest request);
 }
