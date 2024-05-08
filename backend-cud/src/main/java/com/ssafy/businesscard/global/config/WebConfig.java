cf7b8c072e8ae9b090887d42bf1ab80e302c98d9
정준원 <junwon1131@naver.com>
2024-05-08 15:16:34 +0900

    #S10P31S208-131 í-131 cors modify

--- backend-cud/src/main/java/com/ssafy/businesscard/global/config/WebConfig.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/global/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
@@ -13,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedHeaders("*")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns(String.valueOf(Arrays.asList("https://localhost:53000","https://teams.microsoft.com/v2/")))
                 .allowCredentials(true)
 //                .allowedOrigins("*")
                 .allowedMethods("*");
