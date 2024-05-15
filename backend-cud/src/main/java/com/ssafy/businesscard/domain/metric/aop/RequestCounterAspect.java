5bdaa7eabb8b83615b5f14c19e9e0d8a48e053e8
정준원 <junwon1131@naver.com>
2024-05-16 02:01:13 +0900

    aop 주석해제

--- backend-cud/src/main/java/com/ssafy/businesscard/domain/metric/aop/RequestCounterAspect.java
+++ backend-cud/src/main/java/com/ssafy/businesscard/domain/metric/aop/RequestCounterAspect.java
@@ -1,23 +1,23 @@
-//package com.ssafy.businesscard.domain.metric.aop;
-//
-//import io.prometheus.client.Counter;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component
-//public class RequestCounterAspect {
-//
-//    private static final Counter httpRequestsTotal = Counter.build()
-//            .name("http_requests_total")
-//            .help("Total number of HTTP requests")
-//            .register();
-//
-//
-//    @Before("execution(* com.ssafy.businesscard.domain.*.controller.*.*(..))")
-//    public void incrementRequestCounter() {
-//        httpRequestsTotal.inc();
-//    }
-//
-//}
+package com.ssafy.businesscard.domain.metric.aop;
+
+import io.prometheus.client.Counter;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class RequestCounterAspect {
+
+    private static final Counter httpRequestsTotal = Counter.build()
+            .name("http_requests_total")
+            .help("Total number of HTTP requests")
+            .register();
+
+
+    @Before("execution(* com.ssafy.businesscard.domain.*.controller.*.*(..))")
+    public void incrementRequestCounter() {
+        httpRequestsTotal.inc();
+    }
+
+}
